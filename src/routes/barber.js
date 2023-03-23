8561e52b3bcf8f8dd4616f6af5d1ace99c5af3ff
marcelo-de-santana <marcelondesantana@outlook.com>
2023-03-22 23:08:57 -0300

    alterações de consultas da agenda (em andamento)

--- src/routes/barber.js
+++ src/routes/barber.js
@@ -1,14 +1,17 @@
 //DEPENDENCIAS
-    const express = require("express");
-    const router = express.Router();
-    const dbConn = require('../services/mysql');
+const express = require("express");
+const router = express.Router();
+const dbConn = require('../services/mysql');
 
 //ROTAS DO BARBERIO
-    module.exports = router;
+module.exports = router;
 
 /** BUSCAR TODOS OS BARBEIROS **/
-router.get('/', async (req,res,next) => {
-    const result = await dbConn.execute('SELECT * FROM barbeiro')
+router.get('/', async (req, res, next) => {
+
+    const sql = ``
+
+    const result = await dbConn.execute(sql)
 
     res.status(200).send(result)
 })
--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -10,45 +10,52 @@ module.exports = router;
 
 /** BUSCAR AGENDA DA BARBEARIA **/
 router.get('/', async (req, res, next) => {
-    const sql = `SELECT b.id, b.nome, e.horario_inicio, e.horario_fim, e.intervalo_inicio, e.intervalo_fim
-                FROM barbeiros AS b INNER JOIN expediente AS e ON b.id = e.id_barbeiro`
+    const sql = `SELECT b.id, b.nome, h.horario_inicio, h.horario_fim, h.intervalo_inicio, h.intervalo_fim
+                FROM barbeiros AS b INNER JOIN horario_atendimento AS h ON b.id = h.id_barbeiro`
 
-    const allAppointments = await dbConn.execute(sql)
-    const businessHours = await dbConn.execute('SELECT * FROM calendario_diario')
+    const results = await dbConn.execute(sql)
 
-    function getAvailableTimes(values) {
+    results.map(values => {
+        const startTime = new Date(`1970-01-01T${values.horario_inicio}Z`)
+        const endTime = new Date (`1970-01-01T${values.horario_fim}Z`)
+        const startInterval = new Date (`1970-01-01T${values.intervalo_inicio}Z`)
+        const endInterval = new Date(`1970-01-01T${values.intervalo_fim}Z`)
+        const breaks = []
+})})
+    
+/**
 
-        //FILTRAGEM DE HORÁRIOS DISPONÍVEIS
-        const arrFilter = businessHours.filter(item => {
-            if (item.horarios >= values.horario_inicio && item.horarios < values.horario_fim) {
-                return item.horarios
-            }
-        })
-        return arrFilter.map(item => { return item.horarios })
-    }
+    //     return {
+    //         id: values.id,
+    //         name: values.nome,
+    //         available_times: availableTimes,
+    //         unavailable_times: unavailableTimes,
+    //         office_hour: [values.horario_inicio, values.horario_fim],
+    //         office_break: [values.intervalo_inicio, values.intervalo_fim],
 
-    function getUnavailableTimes(values) {
+    //     }
+    // })
 
-        //FILTRAGEM DE HORÁRIOS INDISPONÍVIES
-        const arrFilter = (businessHours.filter(item => {
-            if (item.horarios >= values.intervalo_inicio && item.horarios < values.intervalo_fim || item.horarios == values.horarios_marcados) {
-                return item.horarios
-            }
-        }))
-        return arrFilter.map(item => { return item.horarios })
-    }
+    // // function getAvailableTimes(values) {
 
-    //CORPO DA AGENDA
-    const allResults = allAppointments.map(values => ({
-            id: values.id,
-            name: values.nome,
-            available_times: getAvailableTimes(values),
-            unavailable_times: getUnavailableTimes(values),
-            office_hour: [values.horario_inicio, values.horario_fim],
-            office_break: [values.intervalo_inicio, values.intervalo_fim],
-        }
-    ))
+    // //     //FILTRAGEM DE HORÁRIOS DISPONÍVEIS
+    // //     const arrFilter = businessHours.filter(item => {
+    // //         if (item.horarios >= values.horario_inicio && item.horarios < values.horario_fim) {
+    // //             return item.horarios
+    // //         }
+    // //     })
+    // //     return arrFilter.map(item => { return item.horarios })
+    // // }
 
-    res.status(200).send(allResults)
+    // // function getUnavailableTimes(values) {
 
-})
+    // //     //FILTRAGEM DE HORÁRIOS INDISPONÍVIES
+    // //     const arrFilter = (businessHours.filter(item => {
+    // //         if (item.horarios >= values.intervalo_inicio && item.horarios < values.intervalo_fim || item.horarios == values.horarios_marcados) {
+    // //             return item.horarios
+    // //         }
+    // //     }))
+    // //     return arrFilter.map(item => { return item.horarios })
+    // // }
+    // res.status(200).send(results)
+**/
